23015b3f438151642d51a64546d4eb630229f1de
Jeremy Hughes <23299787+jhughes-dev@users.noreply.github.com>
2023-01-07 14:51:58 -0500

    Formatting

--- src/composite_test.rs
+++ src/composite_test.rs
@@ -1,17 +1,15 @@
 use crate::composite::*;
 
 #[test]
-fn structure()
-{
+fn structure() {
     let mut inner = Composite::new();
     inner.add(Leaf::new("A"));
     inner.add(Leaf::new("B"));
-    assert_eq!(inner.apply(),"(A+B)");
+    assert_eq!(inner.apply(), "(A+B)");
 
     let mut outer = Composite::new();
     outer.add(Leaf::new("C"));
     outer.add(inner);
     outer.add(Leaf::new("D"));
-    assert_eq!(outer.apply(),"(C+(A+B)+D)");
-
+    assert_eq!(outer.apply(), "(C+(A+B)+D)");
 }
