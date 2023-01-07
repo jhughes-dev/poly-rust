2566cdddaef088c45261cb33f7ab83035214f1be
Jeremy Hughes <hughes.jeremy.s@gmail.com>
2023-01-07 14:45:38 -0500

    Rename push to add

--- src/composite_test.rs
+++ src/composite_test.rs
@@ -4,14 +4,14 @@ use crate::composite::*;
 fn structure()
 {
     let mut inner = Composite::new();
-    inner.push(Leaf::new("A"));
-    inner.push(Leaf::new("B"));
+    inner.add(Leaf::new("A"));
+    inner.add(Leaf::new("B"));
     assert_eq!(inner.apply(),"(A+B)");
 
     let mut outer = Composite::new();
-    outer.push(Leaf::new("C"));
-    outer.push(inner);
-    outer.push(Leaf::new("D"));
+    outer.add(Leaf::new("C"));
+    outer.add(inner);
+    outer.add(Leaf::new("D"));
     assert_eq!(outer.apply(),"(C+(A+B)+D)");
 
 }
