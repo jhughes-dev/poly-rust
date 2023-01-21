1b5707fe35d6093b37617bac7afb15030848e09f
Jeremy Hughes <hughes.jeremy.s@gmail.com>
2023-01-21 11:42:54 -0500

    Add example of external command implementation.

--- src/command_test.rs
+++ src/command_test.rs
@@ -16,3 +16,25 @@ fn workflow() {
     // Notice this won't let you create a processor or access data from App
     // let mut proc = Processor {};
 }
+
+
+struct SetToZero {
+
+}
+
+impl Command for SetToZero {
+    fn execute(&self, proc: &mut Processor) {
+        proc.clear();
+    }
+}
+
+#[test]
+fn clear_with_command() {
+    let mut app = App::new();
+
+    app.apply_command(&SetValue::new(5));
+    assert_eq!(app.get_data(), 5);
+
+    app.apply_command(&SetToZero{});
+    assert_eq!(app.get_data(), 0);
+}
